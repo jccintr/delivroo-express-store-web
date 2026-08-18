7cbe30e0697e0f5538a8c8b11c919557dce8d420
Julio Cesar <jccintr@gmail.com>
2026-08-18 17:07:22 -0300

    Instalado dep tests

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,9 @@ import flowbiteReact from 'flowbite-react/plugin/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), flowbiteReact()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/test/setup.js',
+  },
 })
